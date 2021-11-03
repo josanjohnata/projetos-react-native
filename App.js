e094ac49e0e15dd83eeae9ea6e31d4cbdcee256a
Josan Johnata <josanjohnata@gmail.com>
2021-11-03 15:37:51 -0300

    React Native - Basico - Aula 4

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text, Button, Image } from 'react-native';
 
+class ImageComponent extends Component {
+  render() {
+    const image = {
+      uri:'https://conteudo.imguol.com.br/c/entretenimento/c3/2017/11/24/'+this.props.nome+'-einstein-1511565360545_v2_450x337.jpg.webp'
+    }
+    
+    return (
+      <image source={image} style={{width: 300, height: 300}} />
+    );
+  }
+}
+
 export default class App extends Component {
   render() {
     const name = 'Josan Johnata';
-    const image = {
-      uri:'https://conteudo.imguol.com.br/c/entretenimento/c3/2017/11/24/albert-einstein-1511565360545_v2_450x337.jpg.webp'
-    }
 
     return (
       <View>
@@ -16,7 +25,9 @@ export default class App extends Component {
         <Button title="Aperte" onPress={() => {
           alert('Me apertou! ;)')
         }} />
-        <Image source={image} style={{width: 300, height: 300, marginTop: 20}} />
+
+        <ImageComponent nome="albert" />
+
       </View>
     );
   }
