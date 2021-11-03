ed658dbb8444c1f6909913aa938a6d5b9d316178
Josan Johnata <josanjohnata@gmail.com>
2021-11-03 12:29:28 -0300

    React Native - Basico - Aula 2

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default class App extends Component {
   render() {
+    const name = 'Josan Johnata';
+
     return (
       <View>
         <Text>
-          Olá Mundo!
+          Olá! Meu nome é {name}!
         </Text>
+        <Button title="Aperte" onPress={() => {
+          alert('Me apertou! ;)')
+        }} />
       </View>
     );
   }
