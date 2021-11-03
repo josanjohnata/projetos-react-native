a4763877a8e3ece14ecfe592da03407f3da94dda
Josan Johnata <josanjohnata@gmail.com>
2021-11-03 16:38:22 -0300

    React Native - Basico - Aula 5

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text, Button, Image } from 'react-native';
+import { appStyles } from './style/app.style'
 
-class ImageComponent extends Component {
+class Janta extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {comida: props.comida};
+    let comidas = ['Pizza', 'Lasanha', 'Burger', 'Sopa', 'Salada', 'Arroz'];
+
+    setInterval(() => {
+      this.setState(previousState => {
+        let number = Math.floor(Math.random() * comidas.length);
+
+        return {comida: comidas[number]};
+      });
+    }, 1000);
+  }
   render() {
-    const image = {
-      uri:'https://conteudo.imguol.com.br/c/entretenimento/c3/2017/11/24/'+this.props.nome+'-einstein-1511565360545_v2_450x337.jpg.webp'
-    }
-    
     return (
-      <image source={image} style={{width: 300, height: 300}} />
+      <View>
+        <Text style={appStyles.header}>Hoje você vai jantar:</Text>
+        <Text style={appStyles.text}>{this.state.comida}</Text>
+      </View>
     );
   }
 }
 
+
 export default class App extends Component {
   render() {
-    const name = 'Josan Johnata';
-
     return (
-      <View>
-        <Text style={{fontSize:25, color: 'pink', marginTop: 20, marginBottom: 20}}>
-          Olá! Meu nome é {name}!
-        </Text>
-        <Button title="Aperte" onPress={() => {
-          alert('Me apertou! ;)')
-        }} />
-
-        <ImageComponent nome="albert" />
-
+      <View style={{paddingTop:20}}>
+        <Janta comida='Cuscuz' />
       </View>
     );
   }
---
+++ style/app.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const appStyles = StyleSheet.create({
+  header: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'red'
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: 'blue'
+  }
+})
