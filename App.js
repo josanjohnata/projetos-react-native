e4adbfe2c177eacb76e8748f89ef24025728f5a9
Josan Johnata <josanjohnata@gmail.com>
2021-11-04 11:33:07 -0300

    React Native - Basico - Aula 11 e 12 projeto - Criador de mimimi

--- App.js
+++ App.js
@@ -1,30 +1,55 @@
 import React, { Component } from "react";
 import { View, Text, Button, Image, TextInput } from "react-native";
-import { appStyles } from "./style/app.style";
+import { appStyles } from "./src/style/app.style";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {inputTexto: '', texto: ''};
+    this.state = { texto1: 'Texto 1', texto2: 'Texto 2' };
 
-    this.apertouBotao = this.apertouBotao.bind(this);
+    this.escrever = this.escrever.bind(this);
   }
 
-  apertouBotao() {
-    let result = this.state;
-
-    if(result.inputTexto === 'Josan') {
-      result.texto = 'Parabéns! Você acertou!';
-    } else {
-      result.texto = 'Você errou! Tente novamente.'
-    }
-    this.setState(result);
+  mudarVogais(text) {
+    let newText = text.toLowerCase();
+    newText = newText.replace(/(a|e|i|o|u)/g, 'i');
+    newText = newText.replace(/(á|à|ã|â)/g, 'i');
+    newText = newText.replace(/(é|è|ẽ|ê)/g, 'i');
+    newText = newText.replace(/(í|ì|ĩ|î)/g, 'i');
+    newText = newText.replace(/(ó|ò|õ|ô)/g, 'i');
+    newText = newText.replace(/(ú|ù|ũ|û)/g, 'i');
+    return newText;
   }
+
+  escrever(text) {
+    let message = this.state;
+    message.texto1 = text;
+    message.texto2 = this.mudarVogais(text);
+    this.setState(message);
+  }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {inputTexto: '', texto: ''};
+
+  //   this.apertouBotao = this.apertouBotao.bind(this);
+  // }
+
+  // apertouBotao() {
+  //   let result = this.state;
+
+  //   if(result.inputTexto === 'Josan') {
+  //     result.texto = 'Parabéns! Você acertou!';
+  //   } else {
+  //     result.texto = 'Você errou! Tente novamente.'
+  //   }
+  //   this.setState(result);
+  // }
 
   render() {
     return (
-      <View style={{padding: 30}}>
-        <TextInput
+      <View style={appStyles.body}>
+        {/* <TextInput
           style={appStyles.input}
           placeholder="Adivinhe meu NOME?"
           onChangeText={(inputTexto) => this.setState({inputTexto})}
@@ -38,7 +63,27 @@ export default class App extends Component {
           style={appStyles.text}          
         >
           {this.state.texto}
-        </Text>
+        </Text> */}
+
+        {/* Projeto Gerador de memes - para praticar o que foi estudado até agora */}
+        <View>
+          <Text style={appStyles.header}>Criador de Mimimi</Text>
+          <View style={appStyles.inputArea}>
+            <TextInput
+              style={appStyles.input}
+              placeholder="Digite seu Mimimi"
+              onChangeText={this.escrever}
+            />
+          </View>
+          <View style={appStyles.area}>
+            <Text style={[appStyles.texto, appStyles.texto1]}>{this.state.texto1.toUpperCase()}</Text>
+            <Image
+              style={appStyles.image}
+              source={require("./src/images/mimimi.jpg")}
+            />
+            <Text style={[appStyles.texto, appStyles.texto2]}>{this.state.texto2.toUpperCase()}</Text>
+          </View>
+        </View>
       </View>
     );
   }
